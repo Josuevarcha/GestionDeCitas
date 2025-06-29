3de383194d356d7147fc3ad229607d72e01f5cc0
Josuevarcha <163027149+Josuevarcha@users.noreply.github.com>
2025-06-29 07:17:56 -0600

    ds

--- saludApi/Program.cs
+++ saludApi/Program.cs
@@ -1,35 +1,32 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using saludApi.Data;
-using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar controladores
 builder.Services.AddControllers();
 
-// Configurar conexión a la base de datos
 builder.Services.AddDbContext<SaludContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ConexionSql"))
 );
 
-// Configurar CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigins",
         policy =>
         {
             policy.WithOrigins(
-                "http://localhost:8080",                // Frontend local
-                "http://localhost:3000",                // Frontend local
-                "http://127.0.0.1:5500",                // Live Server local
-                "https://gestiondecitasfront.onrender.com" // Frontend desplegado en Render
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "http://127.0.0.1:5500",
+                "https://gestiondecitasfront.onrender.com"// Aquí la URL real de tu frontend
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
         });
 });
 
-// Configurar Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -38,21 +35,11 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Usar Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Usar CORS
 app.UseCors("AllowSpecificOrigins");
-
-// Redirección HTTPS
 app.UseHttpsRedirection();
-
-// Autorización
 app.UseAuthorization();
-
-// Mapear controladores
 app.MapControllers();
-
-// Ejecutar la aplicación
 app.Run();
